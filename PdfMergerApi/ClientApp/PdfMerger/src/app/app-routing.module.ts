fe545c5d1121379509a0988bed03328289d21b2a
radekor500 <radek00500@gmail.com>
2022-02-07 16:53:59 +0100

    styling

--- PdfMergerApi/ClientApp/PdfMerger/src/app/app-routing.module.ts
+++ PdfMergerApi/ClientApp/PdfMerger/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PdfMergeFormComponent } from './pdfTools/pdf-merge-form/pdf-merge-form.component';
+import { HomePageComponent } from './shared/home-page/home-page.component';
 
 const routes: Routes = [
   {path: "merge", component: PdfMergeFormComponent}
+  
 ];
 
 @NgModule({
--- PdfMergerApi/ClientApp/PdfMerger/src/app/app.module.ts
+++ PdfMergerApi/ClientApp/PdfMerger/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PdfMergeFormComponent } from './pdfTools/pdf-merge-form/pdf-merge-form.component';
+import { HomePageComponent } from './shared/home-page/home-page.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PdfMergeFormComponent
+    PdfMergeFormComponent,
+    HomePageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
