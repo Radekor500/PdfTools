5444457eeccd2b083ccb6b66189bc8fc13732330
radekor500 <radek00500@gmail.com>
2022-02-07 14:46:58 +0100

    styling

--- PdfMergerApi/ClientApp/PdfMerger/src/app/pdfTools/pdf-merge-form/pdf-merge-form.component.ts
+++ PdfMergerApi/ClientApp/PdfMerger/src/app/pdfTools/pdf-merge-form/pdf-merge-form.component.ts
@@ -13,36 +13,8 @@ export class PdfMergeFormComponent implements OnInit {
   constructor(private fb: FormBuilder, private pdfService: PdfService) {
     this.mergeForm = new FormGroup({
       endFileName: new FormControl(null),
-      //pdfs: fb.array(File[])
     })
    }
-
-  pushPdf(pdf: any) {
-    const newPdf = new FormControl(pdf, Validators.required);
-    (<FormArray>this.mergeForm.get('pdfs')).push(newPdf)
-  }
-
-  onFileUpload(event: any) {
-    // this.urls = [];
-    let selectedFiles = event.target.files;
-    if (selectedFiles) {
-      for (let file of selectedFiles) {
-        let reader = new FileReader();
-        reader.onload = (e: any) => {
-          // this.urls.push(e.target.result);
-          this.pushPdf(e.target.result);
-        }
-        reader.readAsDataURL(file);
-      }
-    }
-  }
-
-  // onSubmit(data: any) {
-  //   console.log(data);
-  //   this.pdfService.mergePdfs(data).subscribe(resp => {
-  //     console.log(resp);
-  //   })
-  // }
 
   onSubmit() {
     const formData = new FormData();
@@ -53,20 +25,14 @@ export class PdfMergeFormComponent implements OnInit {
       console.log(key);
       formData.append(key, this.mergeForm.value[key]);
     });
-    // formData.append('pdfs', this.mergeForm.get('pdfs')?.value);
-    // files.files.forEach((pdf: any) => {
-    //   formData.append('pdfs', pdf)
-    // })
     Object.keys(files).forEach(key => {
       formData.append('pdfs', files[key]);
     })
-    //formData.append('pdfs', files.files.FileList)
     formData.forEach(item => console.log(item))
     this.pdfService.mergePdfs(formData).subscribe(resp => {
           const blob = new Blob([resp], {type: 'application/pdf'});
           const url = window.URL.createObjectURL(blob);
           window.open(url);
-
         })
   }
   ngOnInit(): void {
